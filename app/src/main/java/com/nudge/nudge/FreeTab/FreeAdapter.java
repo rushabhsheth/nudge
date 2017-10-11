bea44e8f934e07af7cbe07bf77d046d080ab6a61
Rushabh Sheth <rushabh.a.sheth@gmail.com>
2017-10-11 14:54:23 +0530

    1.8 Free tab developed.

---
+++ app/src/main/java/com/nudge/nudge/FreeTab/FreeAdapter.java
@@ -0,0 +1,129 @@
+package com.nudge.nudge.FreeTab;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.nudge.nudge.FriendsTab.FriendsProfile;
+import com.nudge.nudge.R;
+
+import java.util.List;
+
+/**
+ * Created by rushabh on 06/10/17.
+ */
+
+public class FreeAdapter extends RecyclerView.Adapter<FreeAdapter.ViewHolder> {
+
+    private static final String TAG = "FreeAdapter";
+
+    Context mContext;
+    private List<FriendsProfile> mDataSet;
+
+    //Viewholder to hold item_main
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final de.hdodenhof.circleimageview.CircleImageView free_contactsImageView;
+        private final TextView free_nameView;
+        private final ImageButton free_nudgeBtn;
+//        private final ImageButton free_callBtn;
+//        private final ImageButton free_messageBtn;
+
+
+        public ViewHolder(View v) {
+            super(v);
+
+            free_contactsImageView = (de.hdodenhof.circleimageview.CircleImageView) v.findViewById(R.id.free_contactsImageView);
+            free_nameView = (TextView) v.findViewById(R.id.free_nameTextView);
+            free_nudgeBtn = (ImageButton) v.findViewById(R.id.free_nudgeBtn);
+//            free_callBtn = (ImageButton) v.findViewById(R.id.free_callBtn);
+//            free_messageBtn = (ImageButton) v.findViewById(R.id.free_messageBtn);
+        }
+
+        public de.hdodenhof.circleimageview.CircleImageView getFree_contactImageView(){
+            return free_contactsImageView;
+        }
+
+        public TextView getFree_nameView()
+
+        {
+            return free_nameView;
+        }
+        public ImageButton getFree_nudgeBtn(){
+            return free_nudgeBtn;
+        }
+
+//        public ImageButton getFree_callBtn(){
+//            return free_callBtn;
+//        }
+//        public ImageButton getFree_messageBtn(){
+//            return free_messageBtn;
+//        }
+
+    }
+
+    //Constructor
+    public FreeAdapter(Context context, List<FriendsProfile> dataSet) {
+        this.mDataSet = dataSet;
+        this.mContext = context;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view.
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_free, viewGroup, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        //Log.d(TAG, "Element " + position + " set.");
+        final ViewHolder finalHolder = viewHolder;
+        Context context = finalHolder.itemView.getContext();
+        Resources res = finalHolder.itemView.getContext().getResources();
+
+        final FriendsProfile friendsProfile = mDataSet.get(position);
+
+        String userName = friendsProfile.getName();
+        if(userName != null){
+            finalHolder.getFree_nameView().setText(userName);
+        }
+
+
+        String userProfileImage = friendsProfile.getImageUrl();
+        if(userProfileImage!=null){
+            Uri uri = Uri.parse(userProfileImage);
+            Glide.with(context)
+                    .load(uri)
+                    .into(finalHolder.getFree_contactImageView());
+        }
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
+    public void addView(FriendsProfile dataObj, int index) {
+        mDataSet.add(dataObj);
+        notifyItemInserted(index);
+    }
+
+    public void deleteView(int index) {
+        mDataSet.remove(index);
+        notifyItemRemoved(index);
+    }
+
+
+}
--- app/src/main/java/com/nudge/nudge/FreeTab/FreeFragment.java
+++ app/src/main/java/com/nudge/nudge/FreeTab/FreeFragment.java
@@ -1,17 +1,38 @@
 package com.nudge.nudge.FreeTab;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.nudge.nudge.FriendsTab.FriendsProfile;
+import com.nudge.nudge.FriendsTab.FriendsUtils;
 import com.nudge.nudge.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class FreeFragment extends Fragment {
+
+    private static final String TAG = "FreeFragment";
+
+    private Context mContext;
+    private RecyclerView mRecyclerView;
+    private android.support.v7.widget.LinearLayoutManager mLayoutManager;
+    private List<FriendsProfile> mFreeData;
+    private FreeAdapter mFreeAdapter;
+
+
 
     public FreeFragment() {
     }
@@ -19,11 +40,53 @@ public class FreeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mFreeData = new ArrayList<>();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_free, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_free, container, false);
+        rootView.setTag(TAG);
+
+        mContext = rootView.getContext();
+        mRecyclerView = rootView.findViewById(R.id.recyclerview_free);
+
+        int scrollPosition = 0;
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.scrollToPosition(scrollPosition);
+
+        mFreeAdapter = new FreeAdapter(getContext(),mFreeData);
+        mRecyclerView.setAdapter(mFreeAdapter);
+
+//        SnapHelper snapHelper = new LinearSnapHelper();
+//        snapHelper.attachToRecyclerView(mRecyclerView);
+
+        mFreeData = FriendsUtils.loadProfiles(mContext);
+        for(int i = 0; i <mFreeData.size();i++){
+            mFreeAdapter.addView(mFreeData.get(i),i);
+
+        }
+
+        showSwitch(rootView);
+
+        return rootView;
+    }
+
+    private void showSwitch(View rootView){
+        de.hdodenhof.circleimageview.CircleImageView mProfileView = (de.hdodenhof.circleimageview.CircleImageView) rootView.findViewById(R.id.free_selfImageView);
+        int id = getResources().getIdentifier("rushabh_sheth", "drawable", getContext().getPackageName());
+        String path = "android.resource://" + getContext().getPackageName() + "/" + id;
+
+        if(path!=null){
+            Uri uri = Uri.parse(path);
+            Glide.with(mContext)
+                    .load(uri)
+                    .into(mProfileView);
+        }
+
+        TextView free_selfTextView = (TextView) rootView.findViewById(R.id.free_selfTextView);
+        free_selfTextView.setText(mContext.getResources().getString(R.string.stringUserName));
     }
 }
